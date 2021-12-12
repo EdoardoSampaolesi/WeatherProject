d721098b05ad7ea903c9a5cf79c1cb4f9baa7380
EdoardoSampaolesi <94066805+EdoardoSampaolesi@users.noreply.github.com>
2021-12-12 15:55:05 +0100

    Created main classes for cities manager

---
+++ weather/src/main/java/it/project/weather/services/CitiesManagerDaily.java
@@ -0,0 +1,19 @@
+package it.project.weather.services;
+
+import it.project.weather.utils.CitiesManagerImpl;
+
+public class CitiesManagerDaily extends CitiesManagerImpl {
+
+    @Override
+    public String getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getWeather(String city) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
