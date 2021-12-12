4f9a995de092e44a58e43cb56cc2f66ae494f826
EdoardoSampaolesi <94066805+EdoardoSampaolesi@users.noreply.github.com>
2021-12-12 15:25:36 +0100

    Created Controller with first routes

---
+++ weather/src/main/java/it/project/weather/controller/StatsController.java
@@ -0,0 +1,16 @@
+package it.project.weather.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatsController {
+
+    @GetMapping("/stats")
+    public String createStats(@RequestParam(value="example") String[] cities ) //incomplete method
+    {
+        return "example";
+    }
+    
+}
---
+++ weather/src/main/java/it/project/weather/controller/WeatherController.java
@@ -0,0 +1,67 @@
+package it.project.weather.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeatherController 
+{
+
+    @PostMapping
+    public String addCity(@RequestBody String city)
+    {
+        return "example";
+    }
+
+
+    @GetMapping("/current")
+    public String currentWeather(@RequestParam(value="cities") String[] cities)
+    {
+        String example = null;
+        for (String city : cities) {
+            example += city + " ";            
+        }
+        return example;
+    }
+
+    @GetMapping("/hourly")
+    public String hourlyWeather(@RequestParam(value="cities") String[] cities)
+    {
+        String example = null;
+        for (String city : cities) {
+            example += city + " ";            
+        }
+        return example;
+    }
+
+    @GetMapping("/daily")
+    public String dailyWeather(@RequestParam(value="cities") String[] cities)
+    {
+        String example = null;
+        for (String city : cities) {
+            example += city + " ";            
+        }
+        return example;
+    }
+
+    @GetMapping("/dayslot")
+    public String morningDayEveNightWeather(@RequestParam(value="cities") String[] cities)
+    {
+        String example = null;
+        for (String city : cities) {
+            example += city + " ";            
+        }
+        return example;
+    }
+
+    @DeleteMapping
+    public String deleteCity(@RequestBody String[] city)
+    {
+        return "example";
+    }
+ 
+}
