7cea4bd81f73b7f853e1340fbe56d56e2946fa79
EdoardoSampaolesi <94066805+EdoardoSampaolesi@users.noreply.github.com>
2021-12-13 23:31:57 +0100

    Implemented weather methods in weatherController

--- weather/src/main/java/it/project/weather/controller/WeatherController.java
+++ weather/src/main/java/it/project/weather/controller/WeatherController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.project.weather.services.CitiesManagerCurrent;
+import it.project.weather.services.CitiesManagerDaily;
+import it.project.weather.services.CitiesManagerHourly;
+import it.project.weather.services.CitiesManagerMorDayEveNight;
 import it.project.weather.utils.CitiesManagerImpl;
 
 @RestController
@@ -18,51 +21,39 @@ public class WeatherController
     CitiesManagerImpl manager;
 
     @PostMapping
-    public String addCity(@RequestBody String[] city)
+    public String addCity(@RequestBody String[] cities)
     {
         manager = new CitiesManagerCurrent();
-        manager.add(Vector.class.cast(city));
+        manager.add(Vector.class.cast(cities));
         return "successful operation!";
     }
 
     @GetMapping("/current")
     public String currentWeather(@RequestParam(value="cities") String[] cities)
     {
-        String example = null;
-        for (String city : cities) {
-            example += city + " ";            
-        }
-        return example;
+        manager = new CitiesManagerCurrent();
+        return manager.getWeather(Vector.class.cast(cities));
     }
 
     @GetMapping("/hourly")
     public String hourlyWeather(@RequestParam(value="cities") String[] cities)
     {
-        String example = null;
-        for (String city : cities) {
-            example += city + " ";            
-        }
-        return example;
+        manager = new CitiesManagerHourly();
+        return manager.getWeather(Vector.class.cast(cities));
     }
 
     @GetMapping("/daily")
     public String dailyWeather(@RequestParam(value="cities") String[] cities)
     {
-        String example = null;
-        for (String city : cities) {
-            example += city + " ";            
-        }
-        return example;
+        manager = new CitiesManagerDaily();
+        return manager.getWeather(Vector.class.cast(cities));
     }
 
     @GetMapping("/dayslot")
     public String morningDayEveNightWeather(@RequestParam(value="cities") String[] cities)
     {
-        String example = null;
-        for (String city : cities) {
-            example += city + " ";            
-        }
-        return example;
+        manager = new CitiesManagerMorDayEveNight();
+        return manager.getWeather(Vector.class.cast(cities));
     }
 
     @DeleteMapping
