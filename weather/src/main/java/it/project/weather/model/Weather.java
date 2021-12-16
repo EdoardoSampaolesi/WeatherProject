827fe5b48b089a1f905967c64015e47894af5422
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-16 18:01:27 +0100

    Get and setter

--- weather/src/main/java/it/project/weather/model/Weather.java
+++ weather/src/main/java/it/project/weather/model/Weather.java
@@ -52,8 +52,139 @@ public class Weather implements WeatherModelEntity
     @Override
     public JSONObject toJSON() 
     {
-        // TODO Auto-generated method stub
-        return null;
+    	JSONObject att = new JSONObject();
+    	att.put("date", this.date);
+    	att.put("weather", this.main_weather);
+    	att.put("weather descritpion", this.description);
+    	att.put("humidity", this.humidity);
+    	att.put("cloudiness", this.clouds);
+    	att.put("wind speed", this.wind_speed);
+    	att.put("wind direction", this.wind_deg);
+    	att.put("wind type", this.wind_type);
+    	att.put("precipitation volume", this.rain);
+    	att.put("snow volume", this.snow);
+    	att.put("visibility", this.visibility);
+    	att.put("current temperature", this.temp_current);
+    	att.put("feels like temperature", this.temp_feelslike);
+    	att.put("probability percipitation", this.pop_rain);
+    	att.put(" ", this.t);
+    	
+        return att;
     }
+    
+    public Date getDate() 
+    {
+		return date;
+	}
+    public void setDate(Date date) 
+    {
+		this.date = date;
+	}
+    
+    public String getMainwether() 
+    {
+		return main_weather;
+	}
+    public void setMainweather(String main_weather) 
+    {
+		this.main_weather = main_weather;
+	}
+    
+    public String getDescription() 
+    {
+		return description;
+	}
+    public void setDescription(String description) 
+    {
+		this.description = description;
+	}
+    
+    public Double getHumidity() 
+    {
+		return humidity;
+	}
+    public void setDescription(Double humidity) 
+    {
+		this.humidity = humidity;
+	}
+
+	public short getClouds() {
+		return clouds;
+	}
+	public void setClouds(short clouds) {
+		this.clouds = clouds;
+	}
+	
+	public double getWind_speed() {
+		return wind_speed;
+	}
+	public void setWind_speed(double wind_speed) {
+		this.wind_speed = wind_speed;
+	}
+
+	public short getWind_deg() {
+		return wind_deg;
+	}
+	public void setWind_deg(short wind_deg) {
+		this.wind_deg = wind_deg;
+	}
+
+	public String getWind_type() {
+		return wind_type;
+	}
+	public void setWind_type(String wind_type) {
+		this.wind_type = wind_type;
+	}
+
+	public short getRain() {
+		return rain;
+	}
+	public void setRain(short rain) {
+		this.rain = rain;
+	}
+
+	public short getSnow() {
+		return snow;
+	}
+	public void setSnow(short snow) {
+		this.snow = snow;
+	}
+
+	public short getVisibility() {
+		return visibility;
+	}
+	public void setVisibility(short visibility) {
+		this.visibility = visibility;
+	}
+
+	public double getTemp_current() {
+		return temp_current;
+	}
+	public void setTemp_current(double temp_current) {
+		this.temp_current = temp_current;
+	}
+
+	public double getTemp_feelslike() {
+		return temp_feelslike;
+	}
+	public void setTemp_feelslike(double temp_feelslike) {
+		this.temp_feelslike = temp_feelslike;
+	}
+
+	public short getPop_rain() {
+		return pop_rain;
+	}
+	public void setPop_rain(short pop_rain) {
+		this.pop_rain = pop_rain;
+	}
+
+	public Weather_complete getT() {
+		return t;
+	}
+	public void setT(Weather_complete t) {
+		this.t = t;
+	}
+    
+    
     
 }
