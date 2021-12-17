d933d924f54db70a950e38af93c80dc4d8909f2e
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-17 15:14:45 +0100

    Managing exceptions

---
+++ weather/src/main/java/it/project/weather/interfaces/CityInterface.java
@@ -0,0 +1,10 @@
+package it.project.weather.interfaces;
+
+import it.project.weather.exeptions.CityNotFoundException;
+
+public interface CityInterface{
+	
+
+	public void createFromJSON(WeatherService wService) throws CityNotFoundException;
+
+}
