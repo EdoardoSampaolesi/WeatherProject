754658d19a6d21ccb6eaacc78f1d954a1416b4fb
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-18 16:45:57 +0100

    Create toJson and createFromJason methods.

--- weather/src/main/java/it/project/weather/model/City.java
+++ weather/src/main/java/it/project/weather/model/City.java
@@ -2,6 +2,11 @@ package it.project.weather.model;
 
 import org.json.simple.JSONObject;
 
+
+import it.project.weather.exeptions.CityNotFoundException;
+
+import it.project.weather.interfaces.CityInterface;
+
 import it.project.weather.interfaces.WeatherModelEntity;
 import it.project.weather.interfaces.WeatherService;
 import java.util.Date;
@@ -10,29 +15,43 @@ import java.util.Date;
 public class City implements WeatherModelEntity
 {
 	private String name;
-	private CoordImpl coordimpl =new CoordImpl();
-	private Date offset = new Date();
+	private CoordImpl coord;
+	private int offset;
 	
-	public City(String name, CoordImpl coordimpl, Date offset)
+	public City(String name)
     {
         this.name=name;
-        this.coordimpl=coordimpl;
-        this.offset=offset;
     }
 
     @Override
-    public void createFromJSON(WeatherService wService)
+    public void createFromJSON(WeatherService wService) throws CityNotFoundException, Exception
     {
-        //wService.geocodingAPI(this.name);
+    	try 
+    	{
+    		wService.geocodingAPI(name);
+    		JSONObject obj= wService.geocodingAPI(name);
+    		double lat=(double) obj.get("lat");
+    		double lon=(double) obj.get("lon");
+    		this.coord = new CoordImpl(lat, lon); 
+    		offset=(int) obj.get("timezone_offeset");
+    	}
+    	catch(CityNotFoundException e) 
+    	{
+    		throw e;
+    	}
+    	catch(Exception e) 
+    	{
+    		throw e;
+    	}
     }
 
     @Override
     public JSONObject toJSON() 
     {
     	JSONObject att = new JSONObject();
-    	att.put("name", this.name);
-    	att.put("latitude and longitude", this.coordimpl);
-    	att.put("time zone", this.offset);
+    	att.put("Name", this.name);
+    	att.put("Latitude and longitude", this.coord);
+    	att.put("Time zone", this.offset);
         return att;
     }    
     
@@ -47,19 +66,21 @@ public class City implements WeatherModelEntity
     
     public CoordImpl getCoord() 
     {
-		return coordimpl;
+		return coord;
 	}
-    public void setCoord(CoordImpl coordimpl) 
+    public void setCoord(CoordImpl coord) 
     {
-		this.coordimpl = coordimpl;
+		this.coord = coord;
 	}
     
-    public Date getOffset() 
+    public int getOffset() 
     {
 		return offset;
 	}
-    public void setOffset(Date offset) 
+    public void setOffset(int offset) 
     {
 		this.offset = offset;
 	}
+   
+    //metodo toString?
 }
