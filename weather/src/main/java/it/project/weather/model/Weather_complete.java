c461ed4e77bda1d59cf72c2d88ad8cc2cfc0b4d7
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-20 17:51:14 +0100

    Improve the constructor from Weather_complete class.

--- weather/src/main/java/it/project/weather/model/Weather_complete.java
+++ weather/src/main/java/it/project/weather/model/Weather_complete.java
@@ -1,15 +1,19 @@
 package it.project.weather.model;
 
-import java.util.Date;
+import java.util.TimeZone;
+
+import org.json.simple.JSONObject;
+
+import it.project.weather.interfaces.WeatherService;
 
 public class Weather_complete extends Weather {
 	
 	private double temp_max;
 	private double temp_min;
 	
-	public Weather_complete(double temp_max, double temp_min, Date date, String main_weather, String description, short humidity, double clouds, double wind_speed, short wind_deg, String wind_type, double rain, double snow, short visibility, double temp_current, double temp_feelslike, double pop_rain, Weather_complete t) 
+	public Weather_complete(double temp_max, double temp_min, String date, String main_weather, String description, short humidity, double clouds, double wind_speed, short wind_deg, String wind_type, double rain, double snow, short visibility, double temp_current, double temp_feelslike, double pop_rain) 
 	{
-		super(date, main_weather, description, humidity, clouds, wind_speed, wind_deg, wind_type, rain, snow, visibility, temp_current, temp_feelslike, pop_rain, t);
+		super(date, main_weather, description, humidity, clouds, wind_speed, wind_deg, wind_type, rain, snow, visibility, temp_current, temp_feelslike, pop_rain);
 		this.temp_max=temp_max;
 		this.temp_min=temp_min;
 	}
@@ -18,6 +22,13 @@ public class Weather_complete extends Weather {
 		this.temp_max=temp_max;
 		this.temp_min=temp_min;
 	}
+	
+	 public void createFromJSON(JSONObject jobj, TimeZone offset) 
+	 {
+		 super.createFromJSON(jobj, offset);
+		 double temp_max=(double) jobj.get("temp_max");
+	     this.temp_max = temp_max;
+	 }
 
 	public double getTemp_max() 
 	{
