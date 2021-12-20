a33ef8b6265443e34fd60924c09680a824a1018c
EdoardoSampaolesi <94066805+EdoardoSampaolesi@users.noreply.github.com>
2021-12-20 18:22:31 +0100

    Implemented createStats in StatsController class

--- weather/src/main/java/it/project/weather/controller/StatsController.java
+++ weather/src/main/java/it/project/weather/controller/StatsController.java
@@ -1,5 +1,9 @@
 package it.project.weather.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +13,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatsController 
 {
+    private final String DATEFORMAT = "dd/MM/yyyy";
+    private final String HOURFORMAT = "HH:mm:ss";
+
     @GetMapping("/stats")
     public ResponseEntity<String> createStats(@RequestParam(value="exclude",required = false) String[] cities,
                                               @RequestParam(value="bthour", required = false) String bthour,
                                               @RequestParam(value="btdate", required = false) String btdate)
     {
-        //Date date = new Date();
-        //if(bthour != null)
-            
-        return new ResponseEntity<String>("example",HttpStatus.OK);
+        Date startDate,endDate,startTime,endTime;
+        try
+        {
+            btdate.replaceAll(" ", "");
+            startDate = new SimpleDateFormat("["+this.DATEFORMAT+",").parse(btdate);
+            btdate.replace(
+                btdate.subSequence(
+                    0, 
+                    btdate.indexOf(',', 0)
+                    ),
+                "");
+            endDate = new SimpleDateFormat(","+this.DATEFORMAT+"]").parse(btdate);
+            bthour.replaceAll(" ", "");
+            startTime = new SimpleDateFormat("["+this.HOURFORMAT+",").parse(bthour);
+            bthour.replace(
+                bthour.subSequence(
+                    0, 
+                    bthour.indexOf(',', 0)
+                    ),
+                "");
+            endTime = new SimpleDateFormat(","+this.HOURFORMAT+"]").parse(bthour);
+            StatisticsFilter filter = new StatisticsFilter(cities,startDate,endDate,startTime,endTime);
+            return new ResponseEntity<String>(filter.getWeather(),HttpStatus.OK);
+        }
+        catch(Exception e)
+        {
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }           
     }
     
 }
