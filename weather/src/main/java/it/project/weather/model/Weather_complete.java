e18fe977c07a2be7ad3378f9a2a99cdaad60fe10
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-20 16:16:10 +0100

    Create Weather_complete that extends Weather.

--- weather/src/main/java/it/project/weather/model/Weather_complete.java
+++ weather/src/main/java/it/project/weather/model/Weather_complete.java
@@ -7,13 +7,18 @@ public class Weather_complete extends Weather {
 	private double temp_max;
 	private double temp_min;
 	
-	public Weather_complete(double temp_max, double temp_min, Date date, String main_weather, String description, double humidity, short clouds, double wind_speed, short wind_deg, String wind_type, short rain, short snow, short visibility, double temp_current, double temp_feelslike, short pop_rain, Weather_complete t) 
+	public Weather_complete(double temp_max, double temp_min, Date date, String main_weather, String description, short humidity, double clouds, double wind_speed, short wind_deg, String wind_type, double rain, double snow, short visibility, double temp_current, double temp_feelslike, double pop_rain, Weather_complete t) 
 	{
 		super(date, main_weather, description, humidity, clouds, wind_speed, wind_deg, wind_type, rain, snow, visibility, temp_current, temp_feelslike, pop_rain, t);
 		this.temp_max=temp_max;
 		this.temp_min=temp_min;
 	}
 	
+	public Weather_complete(double temp_max, double temp_min) {
+		this.temp_max=temp_max;
+		this.temp_min=temp_min;
+	}
+
 	public double getTemp_max() 
 	{
 		return temp_max;
