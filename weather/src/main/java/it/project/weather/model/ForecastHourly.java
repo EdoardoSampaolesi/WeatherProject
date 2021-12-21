00998d83752d4c6d4da2a1c6c467fb6338c7059a
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-21 10:58:30 +0100

    Implement the createFromJSON method from ForecasHourly class.

--- weather/src/main/java/it/project/weather/model/ForecastHourly.java
+++ weather/src/main/java/it/project/weather/model/ForecastHourly.java
@@ -27,28 +27,51 @@ public class ForecastHourly extends Forecast
     @Override
     public void createFromJSON(WeatherService wService)
     {
-       Weather weather;
- 	   JSONArray obj;
-	   try 
-	   {
-	    obj = (JSONArray) wService.oneCallAPI(city, exclude).get("hourly");
-	    JSONObject o;
-	 	for(int i=0; i<24;i++)
-	 	{
-	 		   o= (JSONObject) obj.get(i);
-	 		   weather = new Weather();
-	 	 	   double temp=(double) o.get("temp");
-	 	 	   weather.setTemp_current(temp);
-	 	 	  
-	 	 	   double temp_feels=(double) o.get("feels_like");
-		 	   weather.setTemp_current(temp_feels);
-		 	   weatherList.add(weather);
-	 	 }
+        Weather weather;
+ 	    JSONArray obj;
+	    try 
+	    {
+		    obj = (JSONArray) wService.oneCallAPI(city.getCoord(), exclude).get("hourly");
+	        JSONObject o;
+	 	    for(int i=0; i<24;i++)
+	 	    {
+	 		    o = (JSONObject) obj.get(i);
+	 		    weather = new Weather();
+	 		   
+	 		    String main=(String) o.get("main");
+	 		    weather.setMainweather(main);
+	 		    String description=(String) o.get("description");
+	 		    weather.setDescription(description);
+	 		    double humidity=(double) o.get("humidity");
+	 	 	    weather.setHumidity(humidity);
+	 	 	    double cloudiness=(double) o.get("clouds");
+	 	 	    weather.setClouds(cloudiness);
+	 	 	    double wind_speed=(double) o.get("wind_speed");
+	 	 	    weather.setWind_speed(wind_speed);
+	 	 	    short wind_deg=(short) o.get("wind_deg");
+	 	 	    weather.setWind_deg(wind_deg);
+	 	 	    //String wind_type=(String) o.get("wind_deg");
+	 	 	    //weather.setWind_deg(wind_deg);
+	 	 	    double rain=(double) o.get("rain");
+	 	 	    weather.setRain(rain);
+	 	 	    double snow=(double) o.get("snow");
+	 	 	    weather.setSnow(snow);
+	 	 	    short vis=(short) o.get("visibility");
+	 	 	    weather.setWind_deg(vis);
+	 	 	    double temp=(double) o.get("temp");
+	 	 	    weather.setTemp_current(temp);
+	 	 	    double temp_feels=(double) o.get("feels_like");
+	 	 	    weather.setTemp_feelslike(temp_feels);
+	 	 	    double pop_rain=(double) o.get("pop");
+	 	 	    weather.setPop_rain(pop_rain);
+	 	 	   
+	 	 	    weatherList.add(weather);
+	 	   }
 	   } 
-		catch (IOException | ParseException e)
-		{
+	   catch (IOException | ParseException e)
+	   {
 			e.printStackTrace();
-		} 
+	   } 
 		
  	   
     }
@@ -56,7 +79,8 @@ public class ForecastHourly extends Forecast
     @Override
     public JSONObject toJSON() 
     {
-        // TODO Auto-generated method stub
+    	
+        
         return null;
     }
     
