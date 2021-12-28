97a775ece40ee0cbdffc5c70c618577725bdb61d
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-28 11:23:42 +0100

    Added fromCityOffsetToGMT method to WeatherModelEntity interface.

--- weather/src/main/java/it/project/weather/interfaces/WeatherModelEntity.java
+++ weather/src/main/java/it/project/weather/interfaces/WeatherModelEntity.java
@@ -1,5 +1,8 @@
 package it.project.weather.interfaces;
 
+import java.text.ParseException;
+import java.util.Calendar;
+
 import org.json.simple.JSONObject;
 
 import it.project.weather.exeptions.CityNotFoundException;
@@ -8,4 +11,5 @@ public interface WeatherModelEntity
 {
     public void createFromJSON(WeatherService wService) throws CityNotFoundException, Exception;
     public JSONObject toJSON();
+    public Calendar fromCityOffsetToGMT(Calendar date) throws ParseException;
 }
