2c36a9c1fbe41c231bab2ce9b2dfaea5b4408c1f
Matteo Seresi <s1100028@studenti.univpm.it>
2021-12-30 15:42:42 +0100

    WeatherModelEntyty's name changed to CityInterface.

--- weather/src/main/java/it/project/weather/interfaces/CityInterface.java
+++ weather/src/main/java/it/project/weather/interfaces/CityInterface.java
@@ -1,9 +1,15 @@
 package it.project.weather.interfaces;
 
+import java.text.ParseException;
+import java.util.Calendar;
+
+import org.json.simple.JSONObject;
+
 import it.project.weather.exeptions.CityNotFoundException;
 
-public interface CityInterface{
-
-	public void createFromJSON(WeatherService wService) throws CityNotFoundException;
-
+public interface CityInterface
+{
+    public void createFromJSON(WeatherService wService) throws CityNotFoundException, Exception;
+    public JSONObject toJSON();
+    public Calendar fromCityOffsetToMyDate(Calendar date) throws ParseException;
 }
