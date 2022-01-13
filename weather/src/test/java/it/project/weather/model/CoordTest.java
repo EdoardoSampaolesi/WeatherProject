418f5b0a2037dd7971fc5949b15ef6aa4f1be0c0
Matteo Seresi <s1100028@studenti.univpm.it>
2022-01-13 18:35:52 +0100

    Test for coordinates.

---
+++ weather/src/test/java/it/project/weather/model/CoordTest.java
@@ -0,0 +1,30 @@
+package it.project.weather.model;
+
+/**
+ * @author @MatteoSeresi
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import it.project.weather.interfaces.Coord;
+
+public class CoordTest {
+	
+    private Coord coord;
+    private String s;
+    
+    @Before
+    public void setUp()
+    {    
+        coord = new CoordImpl(40.71,-74.00); //new york    
+    }
+    
+    @Test
+    public void testCoordinates()
+    {
+    	s="lat=40.71&lon=-74.00";
+    	assertEquals(s,coord.toString());
+    }
+
+}
